b76a0dc8e73cb014e7c0bc97fe43675cf50860ad
Jove Zhong <jzhong@splunk.com>
2013-11-20 22:13:08 +0800

    add bootstrap, add form, file based store

--- classmates.js
+++ classmates.js
@@ -1,19 +1,35 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to classmates.";
+  Template.survey.items = function () {
+    return [
+      {label:'姓名',key:'姓名',value:''},
+      {label:'性别',key:'性别',value:''},
+    ];
   };
+  function submitForm(){
+    data={};
+    $.each($('#myform').serializeArray(),function(){
+      data[this.name]=this.value;
+    });
+    Meteor.call('saveData',data);
+  }
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.survey.events({
+    'submit' : function () {
+      submitForm();
+      event.preventDefault();
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    fs = Npm.require('fs');
+  });
+  Meteor.methods({
+    saveData: function(data){
+      data.time=new Date();
+      var str=JSON.stringify(data)+"\n";
+      fs.appendFileSync('/Users/jzhong/Dev/git_projects/classmates/private/data.txt',str,'utf8');
+    }
   });
 }
